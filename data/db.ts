7e5ecf6d630cdad19ef33c726a0f25785fde64c9
Naadiya Ahmedabadi <naadiya.ahmedabadi@gmail.com>
2025-01-08 21:39:50 -0800

    Fixed db functions

--- data/db.ts
+++ data/db.ts
@@ -1,80 +1,46 @@
-import { collection, doc, addDoc, getDoc, getDocs, updateDoc } from 'firebase/firestore';
+import {
+  collection,
+  doc,
+  addDoc,
+  getDoc,
+  getDocs,
+  updateDoc,
+} from 'firebase/firestore';
 import { db } from '@/firebase-config';
-import { Project, ProjectType } from '@/lib/types';
+import { Project } from '@/lib/types';
 
 export async function createProject(project: Omit<Project, 'id'>) {
-  try {
-    const projectRef = collection(db, 'projects');
-    const docRef = await addDoc(projectRef, project);
-    console.log('Project added with ID:', docRef.id);
-    return docRef.id;
-  } catch (error) {
-    console.error('Error adding project:', error);
-    throw error;
-  }
+  const projectRef = collection(db, 'projects');
+  const docRef = await addDoc(projectRef, project);
+  return docRef.id;
 }
 
 export async function updateProject(project: Project) {
-  const docRef = doc(db, "projects", project.id);
+  const docRef = doc(db, 'projects', project.id);
   await updateDoc(docRef, project);
-  console.log("Document updated");
+  return project.id;
 }
 
 export async function getProjectById(id: string) {
-  try {
-    const ref = doc(db, 'projects', id);
-    const projectSnapshot = await getDoc(ref);
+  const ref = doc(db, 'projects', id);
+  const projectSnapshot = await getDoc(ref);
 
-    if (projectSnapshot.exists()) {
-      const project = { id, ...projectSnapshot.data() } as Project;
-      console.log('Retrieved project:', project);
-      return project;
-    } else {
-      console.log('No such project exists!');
-      return null;
-    }
-  } catch (error) {
-    console.error('Error retrieving project:', error);
-    throw error;
+  if (projectSnapshot.exists()) {
+    const project = { id, ...projectSnapshot.data() } as Project;
+    return project;
+  } else {
+    console.log('No such project exists!');
+    return null;
   }
 }
 
-// TODO
 export async function getAllProjects() {
-  // try {
-  //   const projectsRef = collection(db, 'projects');
-  //   const querySnapshot = await getDocs(projectsRef);
+  const projectsRef = collection(db, 'projects');
+  const querySnapshot = await getDocs(projectsRef);
 
-  //   const projects: Project[] = [];
-  //   querySnapshot.forEach((doc) => {
-  //     projects.push({ id: doc.id, ...doc.data() } as Project);
-  //   });
-
-  //   console.log('Retrieved all projects:', projects);
-  //   return projects;
-  // } catch (error) {
-  //   console.error('Error retrieving all projects:', error);
-  //   throw error;
-  // }
-
-  return [
-    {
-      id: '1',
-      name: 'Socks',
-      type: ProjectType.Crochet,
-      hooks: '4mm',
-      yarn: 'Wool',
-      notes: '',
-      counters: [{id: '1', name: 'Heel', value: 2}]
-    },
-    {
-      id: '2',
-      name: 'Sweater',
-      type: ProjectType.Knit,
-      hooks: '5mm',
-      yarn: 'Cotton',
-      notes: 'size small',
-      counters: [],
-    },
-  ];
+  const projects: Project[] = [];
+  querySnapshot.forEach((doc) => {
+    projects.push({ id: doc.id, ...doc.data() } as Project);
+  });
+  return projects;
 }
